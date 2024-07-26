dcc538a1e360153d4ef95d4c2a1477c7fc35916d
KingCreatBug <trunghoang.240500@gmail.com>
2024-07-26 20:18:28 +0700

    Hoan thanh giao dien react chat

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,24 @@
+//Call API
+const loginAPI = "";
+fetch(loginAPI)
+    .then(function (response) {
+        return response.json();
+        //JSON --> Javascript type
+    })
+    .then(function (posts) {
+        var htmls = posts.map(function (post) {
+            return `<li>
+                <h2>${post.title}</h2>
+                <p>${post.body}</p>
+            </li>`;
+        });
+        var html = htmls.join("");
+        document.getElementById("post-block").innerHTML = html;
+    })
+    .catch(function (err) {
+        console.log("Co loi!");
+    });
+
 /**
  * JS toggle
  *
